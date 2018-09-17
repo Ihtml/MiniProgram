f9d81746da16573a8cb65d930dbfc6d90a14c4d4
IFE2 <2483690020@qq.com>
2018-09-17 18:17:33 +0800

    add news-item-template

--- Reader/pages/news/news.js
+++ Reader/pages/news/news.js
@@ -1,8 +1,10 @@
 // pages/news/news.js
+var postsData = require('../../data/posts-data.js')
 Page({
 
   /**
-   * 页面的初始数据
+   * 小程序总是会读取data对象来做数据绑定，这个动作称为动作A
+   * 而这个动作A的执行，是在onLoad函数执行之后发生的
    */
   data: {
   
@@ -11,56 +13,27 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function () {
+    // this.data.postList = postsData.postList
+    this.setData({
+      postList: postsData.postList
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  onSwiperTap: function (event) {
+    // target 和currentTarget
+    // target指的是当前点击的组件 currentTarget 指的是事件捕获的组件
+    // target这里指的是image，而currentTarget指的是swiper
+    var postId = event.target.dataset.postid
+    wx.navigateTo({
+      url: 'post-detail/post-detail?id=' + postId
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
   
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  onPostTap: function (event) {
+    var postId = event.currentTarget.dataset.postid;
+    wx.navigateTo({
+      url: "post-detail/post-detail?id=" + postId
+    })
   }
 })
